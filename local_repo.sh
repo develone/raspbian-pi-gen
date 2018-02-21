9cec80af8951eef4353ea0c69251ba2bce1e5451
Edward Vidal <develone@sbcglobal.net>
2018-02-21 13:12:31 -0700

    cmake was needed to build the Ultibo Userland started looking at creating a local repository to speed up builds pkgs count is 1427

---
+++ local_repo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cp ~/pi-gen/work/2018-02-21-RaspbianUltibo/stage0/rootfs/var/cache/apt/archives/*.deb ~/local-apt-repo/
+ls ~/pi-gen/work/2018-02-21-RaspbianUltibo/stage0/rootfs/var/cache/apt/archives/*.deb | wc
+
+cp ~/pi-gen/work/2018-02-21-RaspbianUltibo/stage1/rootfs/var/cache/apt/archives/*.deb ~/local-apt-repo/
+ls ~/pi-gen/work/2018-02-21-RaspbianUltibo/stage1/rootfs/var/cache/apt/archives/*.deb | wc
+
+cp ~/pi-gen/work/2018-02-21-RaspbianUltibo/stage2/rootfs/var/cache/apt/archives/*.deb ~/local-apt-repo/
+ls ~/pi-gen/work/2018-02-21-RaspbianUltibo/stage2/rootfs/var/cache/apt/archives/*.deb | wc
+
+cp ~/pi-gen/work/2018-02-21-RaspbianUltibo/stage3/rootfs/var/cache/apt/archives/*.deb ~/local-apt-repo/
+ls ~/pi-gen/work/2018-02-21-RaspbianUltibo/stage3/rootfs/var/cache/apt/archives/*.deb | wc
+
+cp ~/pi-gen/work/2018-02-21-RaspbianUltibo/stage4/rootfs/var/cache/apt/archives/*.deb ~/local-apt-repo/
+ls ~/pi-gen/work/2018-02-21-RaspbianUltibo/stage4/rootfs/var/cache/apt/archives/*.deb | wc
+
+cp ~/pi-gen/work/2018-02-21-RaspbianUltibo/stage5/rootfs/var/cache/apt/archives/*.deb ~/local-apt-repo/
+ls ~/pi-gen/work/2018-02-21-RaspbianUltibo/stage5/rootfs/var/cache/apt/archives/*.deb | wc
