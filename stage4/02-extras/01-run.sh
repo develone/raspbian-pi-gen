88cfd21671b4df373f88e6f5b83ee62ac2c2300b
Edward Vidal <develone@sbcglobal.net>
2018-03-21 17:51:30 -0600

    This version built in preparation of new RPi3B+ This is using the latest version of Core 03/19/18 from ultibohub.

--- stage4/02-extras/01-run.sh
+++ stage4/02-extras/01-run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -e
 
-tar xfz files/ultibo94af3cb.tgz -C ${ROOTFS_DIR}/home/pi/
+tar xfz files/ultibo2fc3634.tgz -C ${ROOTFS_DIR}/home/pi/
 chown 1000:1000 ${ROOTFS_DIR}/home/pi/ultibo -Rv
 cp files/ultibo.desktop ${ROOTFS_DIR}/home/pi/.local/share/applications
 chown 1000:1000 ${ROOTFS_DIR}/home/pi/.local/share/applications/ultibo.desktop
+cp ${ROOTFS_DIR}/etc/dphys-swapfile ${ROOTFS_DIR}/etc/dphys-swapfile.orig
+sed s/100/1000/ < ${ROOTFS_DIR}/etc/dphys-swapfile.orig > ${ROOTFS_DIR}/etc/dphys-swapfile
+
