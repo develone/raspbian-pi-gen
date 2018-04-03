ac6ca0600b5991c0f9491fa354cec2d9f5c783d9
Edward Vidal <develone@sbcglobal.net>
2018-04-02 21:35:53 -0600

    image-2018-03-31

--- stage4/02-extras/01-run.sh
+++ stage4/02-extras/01-run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 
-tar xfz files/ultibo2fc3634.tgz -C ${ROOTFS_DIR}/home/pi/
+tar xfz files/ultibo75c2ec2.tgz -C ${ROOTFS_DIR}/home/pi/
 chown 1000:1000 ${ROOTFS_DIR}/home/pi/ultibo -Rv
 cp files/ultibo.desktop ${ROOTFS_DIR}/home/pi/.local/share/applications
 chown 1000:1000 ${ROOTFS_DIR}/home/pi/.local/share/applications/ultibo.desktop
 cp ${ROOTFS_DIR}/etc/dphys-swapfile ${ROOTFS_DIR}/etc/dphys-swapfile.orig
 sed s/100/1000/ < ${ROOTFS_DIR}/etc/dphys-swapfile.orig > ${ROOTFS_DIR}/etc/dphys-swapfile
+cp ${ROOTFS_DIR}/etc/default/keyboard ${ROOTFS_DIR}/etc/default/keyboard.orig
+sed s/gb/us/ < ${ROOTFS_DIR}/etc/default/keyboard.orig > ${ROOTFS_DIR}/etc/default/keyboard
 
